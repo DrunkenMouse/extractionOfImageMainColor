ee8919fd4189faeaba70d06f14792cb3cdda3d9d
残月 <632342527@qq.com>
2016-11-23 10:08:17 +0800

    Initial Commit

---
+++ 图像主要颜色的提取/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  图像主要颜色的提取
+//
+//  Created by 王奥东 on 16/11/23.
+//  Copyright © 2016年 王奥东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
